df7ac290d3c8e74db53623dfd8ce568c9b627034
Jemi-code <jemimahkwakuyi@gmail.com>
2023-09-08 02:36:46 +0000

    first commit

---
+++ sanity-project/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '73y5f4i2',
+    dataset: 'production'
+  }
+})
---
+++ src/utils/configSanity.ts
@@ -0,0 +1,8 @@
+import {createClient} from 'next-sanity';
+
+export const client = createClient({
+    projectId: '73y5f4i2',
+    dataset: 'production',
+    apiVersion: '2021-10-21',
+    useCdn: true,
+})
